c36989c130030e9e01acc8e7335d2bf24e79cf8b
Mikhail Poluboyarinov <mikle.sol@gmail.com>
2019-07-12 10:08:59 +0300

    linter fix

--- server/index.js
+++ server/index.js
@@ -112,9 +112,11 @@ server.on('error', (error) => {
     case 'EACCES':
       console.error(`${bind} requires elevated privileges`);
       process.exit(1);
+      break;
     case 'EADDRINUSE':
       console.error(`${bind} is already in use`);
       process.exit(1);
+      break;
     default:
       throw error;
   }
